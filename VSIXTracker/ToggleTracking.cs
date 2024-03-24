7ac9397be411d6cee0845e195d5682d9343f15a9
chris <chris@BORG>
2024-03-24 11:35:36 -0500

    Add project files.

---
+++ VSIXTracker/ToggleTracking.cs
@@ -0,0 +1,135 @@
+﻿using EnvDTE;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using System;
+using System.ComponentModel.Design;
+using System.Globalization;
+using System.Threading;
+using System.Threading.Tasks;
+using Task = System.Threading.Tasks.Task;
+
+namespace VSIXTracker
+{
+    /// <summary>
+    /// Command handler
+    /// </summary>
+    internal sealed class ToggleTracking
+    {
+        /// <summary>
+        /// Command ID.
+        /// </summary>
+        public const int CommandId = 0x0100;
+
+        /// <summary>
+        /// Command menu group (command set GUID).
+        /// </summary>
+        public static readonly Guid CommandSet = new Guid("2b6b9b93-c7fa-4486-9f7c-f6fdd6299e74");
+
+        /// <summary>
+        /// VS Package that provides this command, not null.
+        /// </summary>
+        private readonly AsyncPackage package;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ToggleTracking"/> class.
+        /// Adds our command handlers for menu (commands must exist in the command table file)
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        /// <param name="commandService">Command service to add command to, not null.</param>
+        private ToggleTracking(AsyncPackage package, OleMenuCommandService commandService)
+        {
+            this.package = package ?? throw new ArgumentNullException(nameof(package));
+            commandService = commandService ?? throw new ArgumentNullException(nameof(commandService));
+
+            var menuCommandID = new CommandID(CommandSet, CommandId);
+            var menuItem = new MenuCommand(this.Execute, menuCommandID);
+            commandService.AddCommand(menuItem);
+        }
+
+        /// <summary>
+        /// Gets the instance of the command.
+        /// </summary>
+        public static ToggleTracking Instance
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Gets the service provider from the owner package.
+        /// </summary>
+        private Microsoft.VisualStudio.Shell.IAsyncServiceProvider ServiceProvider
+        {
+            get
+            {
+                return this.package;
+            }
+        }
+
+        /// <summary>
+        /// Initializes the singleton instance of the command.
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        public static async Task InitializeAsync(AsyncPackage package)
+        {
+            // Switch to the main thread - the call to AddCommand in ToggleTracking's constructor requires
+            // the UI thread.
+            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync(package.DisposalToken);
+
+            OleMenuCommandService commandService = await package.GetServiceAsync(typeof(IMenuCommandService)) as OleMenuCommandService;
+            Instance = new ToggleTracking(package, commandService);
+        }
+
+        /// <summary>
+        /// This function is the callback used to execute the command when the menu item is clicked.
+        /// See the constructor to see how the menu item is associated with this function using
+        /// OleMenuCommandService service and MenuCommand class.
+        /// </summary>
+        /// <param name="sender">Event sender.</param>
+        /// <param name="e">Event args.</param>
+        private void Execute(object sender, EventArgs e)
+        {
+
+            var dte = (DTE)Package.GetGlobalService(typeof(SDTE));
+
+            //var myCommand = sender as OleMenuCommand;
+            //if (myCommand == null)
+            //{
+            //    return;
+            //}
+
+            // Get the Properties collection of the DTE object.
+            Properties properties = dte.Properties["Environment", "ProjectsAndSolution"];
+
+            // Iterate over each property in the collection.
+            foreach (Property property in properties)
+            {
+                // Print the property name and value to the debug window.
+                System.Diagnostics.Debug.WriteLine($"Name: {property.Name}, Value: {property.Value}");
+            }
+
+            // Find the "Track Active Item in Solution Explorer" property.
+            Property trackActiveItemPropertyOption = properties.Item("TrackFileSelectionInExplorer");
+
+            if (trackActiveItemPropertyOption != null)
+            {
+                bool currentValue = (bool)trackActiveItemPropertyOption.Value;
+                trackActiveItemPropertyOption.Value = !currentValue;
+
+
+            }
+            //ThreadHelper.ThrowIfNotOnUIThread();
+            //string message = string.Format(CultureInfo.CurrentCulture, "Inside {0}.MenuItemCallback()", this.GetType().FullName);
+            //string title = "ToggleTracking";
+
+            //// Show a message box to prove we were here
+            //VsShellUtilities.ShowMessageBox(
+            //    this.package,
+            //    message,
+            //    title,
+            //    OLEMSGICON.OLEMSGICON_INFO,
+            //    OLEMSGBUTTON.OLEMSGBUTTON_OK,
+            //    OLEMSGDEFBUTTON.OLEMSGDEFBUTTON_FIRST);
+        }
+    }
+}
